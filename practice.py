4f98746aa3497c5e490610381aaf5447677d0e1d
Benjamin R Everett <benjaminreverett@Benjamins-MacBook-Air.local>
2017-07-29 15:24:51 -0600

    update

---
+++ practice.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+X = np.linspace(-np.pi, np.pi, 256, endpoint=True)
+C, S = np.cos(X), np.sin(X)
+
+print(X)
+
+# plt.plot(X, C)
+# plt.plot(X, S)
+
+# plt.show()
